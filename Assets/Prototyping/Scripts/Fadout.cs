047016e4e8a2749bb5321a293a16beb34744c425
Rohith Maddali <1015083@student.sae.edu.au>
2021-07-11 22:23:53 +1000

    Creating a prototyping folder and added some prototyping.

---
+++ Assets/Prototyping/Scripts/Fadout.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fadout : MonoBehaviour
+{
+    public Animator skybox, beach, ocean;
+    public int skyTime, beachTime, oceanTime;
+    public bool sky, oceanSpawned, beachSpawned, everythingSpawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator spawn()
+    {
+        yield return new WaitForSeconds(skyTime);
+        skybox.SetBool("Spawn", true);
+        yield return new WaitForSeconds(beachTime);
+        beach.SetBool("Spawn", true);
+        yield return new WaitForSeconds(oceanTime);
+        ocean.SetBool("Spawn", true);
+    }
+}
