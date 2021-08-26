b8b28a09238fd110628e86fe9c52dae8c62d8e2a
KyanRowse <kyanrowsesounddesign@gmail.com>
2021-08-26 11:40:15 +1000

    Treasure Chest

---
+++ Assets/Audio/AudioScripts/AnimationAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationAudio : MonoBehaviour
+{
+    AudioSource source;
+    [SerializeField] AudioClip clip;
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    public void PlayAnimationSound()
+    {
+        source.PlayOneShot(clip);
+    }
+}
